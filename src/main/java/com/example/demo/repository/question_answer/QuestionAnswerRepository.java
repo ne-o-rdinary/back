10111d0ea7a1583bb32d27e1b4fce1070db51eea
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-11-23 22:33:51 +0900

    refactor : questionanswerrepo 해결

--- src/main/java/com/example/demo/repository/question_answer/QuestionAnswerRepository.java
+++ src/main/java/com/example/demo/repository/question_answer/QuestionAnswerRepository.java
@@ -5,4 +5,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionAnswerRepository extends JpaRepository<QuestionAnswer, Long> {
+
+
 }
